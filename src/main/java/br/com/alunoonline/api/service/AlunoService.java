be3a4f2846cd870c9643b3063da564a2161b9232
robertobzrr <robertobezerrafh@outlook.com>
2025-09-09 16:59:03 -0300

    first commit

---
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -0,0 +1,19 @@
+package br.com.alunoonline.api.service;
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+    //de onde vem esse atributo 'alunoRepository'
+
+    public void criarAluno(Aluno aluno){
+        alunoRepository.save(aluno);
+    }
+
+}
